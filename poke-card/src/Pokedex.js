14e2c7a15d05e284ffdcaf031a982341c7f697d7
TheJSGirl <prinegi100@gmail.com>
2019-05-26 02:33:51 +0530

    added css styling for PokeCard & Pokedex

--- poke-card/src/Pokedex.js
+++ poke-card/src/Pokedex.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PokeCard from './PokeCard';
+import './Pokedex.css';
 
 class Pokedex extends Component {
     static defaultProps = {
@@ -9,14 +10,29 @@ class Pokedex extends Component {
        { id:25,name:"Pikachu",    type:"electric", base_experince:112},
        { id:4, name:"Charmander", type:"fire",     base_experince:62},
        { id:11,name:"Metapod",    type:"bug",     base_experince:72},
+       { id:25,name:"Pikachu",    type:"electric", base_experince:112},
+       { id:4, name:"Charmander", type:"fire",     base_experince:62},
+       { id:11,name:"Metapod",    type:"bug",     base_experince:72},
+       { id:25,name:"Pikachu",    type:"electric", base_experince:112},
+       { id:4, name:"Charmander", type:"fire",     base_experince:62},
+       { id:11,name:"Metapod",    type:"bug",     base_experince:72},
+       { id:25,name:"Pikachu",    type:"electric", base_experince:112},
+       { id:4, name:"Charmander", type:"fire",     base_experince:62},
+       { id:11,name:"Metapod",    type:"bug",     base_experince:72},
+       { id:25,name:"Pikachu",    type:"electric", base_experince:112},
+       { id:4, name:"Charmander", type:"fire",     base_experince:62},
+       { id:11,name:"Metapod",    type:"bug",     base_experince:72},
        { id:25,name:"Pikachu",    type:"electric", base_experince:112}
     ]
     };
     render() {
-        return (<div>
-            {this.props.pokemon.map((p) => (
+        return (<div className="Pokedex">
+           <h1>Pokedex!</h1>
+           <div className="Pokedex-cards">
+           {this.props.pokemon.map((p) => (
                 <PokeCard id={p.id} name={p.name} type={p.type} base_experince={p.base_experince}/>
             ))}
+           </div>
         </div>)
     }
 
