ed299b0c2c25aaeafd6221b656f6eb8e5da2470c
TheJSGirl <prinegi100@gmail.com>
2019-05-26 13:04:05 +0530

    replaced POKE_API & added logic for three digits

--- poke-card/src/PokeCard.js
+++ poke-card/src/PokeCard.js
@@ -1,18 +1,19 @@
 import React, {Component} from  'react';
 import './PokeCard.css';
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number); 
 
 class Pokecard extends Component {
     render() {
         const {name, id, type, base_experince} = this.props;
-        const imgSource = `${POKE_API}${id}.png`;
+        const imgSource = `${POKE_API}${padToThree(id)}.png`;
         return <div className="PokeCard">
-            <h1>{name}</h1>
+            <h1 className="PokeCard-title">{name}</h1>
             <img src={imgSource} alt={name}/>
-            <div>
+            <div className="PokeCard-data">
                 Type: {type}
             </div>
-            <div>
+            <div className="PokeCard-data">
                 EXP: {base_experince}
             </div>
         </div>
